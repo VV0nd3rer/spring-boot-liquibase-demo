240e16f9874da36caebfd780160012429d77c232
kverchi <kverchi24@gmail.com>
2017-10-10 15:00:31 +0200

    Init commit

---
+++ src/main/java/kverchi/learning/springbootliquibasedemo/SpringBootLiquibaseDemoApplication.java
@@ -0,0 +1,12 @@
+package kverchi.learning.springbootliquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLiquibaseDemoApplication.class, args);
+	}
+}
